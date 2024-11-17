a7d7ce19515f0831a498d2abb3f6af60caa8a609
Vladislav Atanassov <vlavalat@gmail.com>
2024-11-17 16:38:49 +0200

    - Separated functionality _rx_loop and _tx_loop - _main_loop made cleaner - Added flags and variables for use in the program - Using _push_vars_stack to push the variables and flags into the stack

--- motorola_6800_assembler/test_files_UART/asm_loop_back_comm.asm
+++ motorola_6800_assembler/test_files_UART/asm_loop_back_comm.asm
@@ -11,43 +11,74 @@ UART_CODE_THRE  equ $02    *; Code for UART Transmiter Holding register Empty
 UART_CODE_RDA   equ $04    *; Code for Recieved Data Available
 UART_CODE_ERR   equ $06    *; Code for UART error in IIR
 
-BUFFER_SIZE_MAX equ $ff     *; Define buffer size   
-BUFFER_CURR_CAP equ $ff     *; Define current buffer capacity variable 
+*; Define variables and flags needed for the program
+BUFFER_SIZE_MAX equ $ff     *; Define buffer max size   
+buffer_curr_cap equ $ff     *; Define current buffer capacity variable
+BUFFER_C_C_ADDR equ $1fff   *; Define where buffer_curr_cap is stored in the SRAM
+buffer_full     equ $00     *; Define a flag to indicate if the buffer is filled
+BUFFER_F_ADDR   equ $1ffe   *; Define where buffer_full is stored in the SRAM
+
 SRAM_ADDR       equ $0000   *; Define SRAM address for storing received data
+SP_ADDR         equ $1fff   *; Define the SP address
 
-    org $1C00
+rx_done         equ $00     *; Define flag for indicating if recieve has ended (0-F, 1-T)
+tx_done         equ $00     *; Define flag for indicating if transmit has ended (0-F, 1-T)
+RX_DONE_ADDR    equ $1ffd   *; Define where rx_done is stored in the SRAM
+TX_DONE_ADDR    equ $1ffc   *; Define where rx_done is stored in the SRAM
 
-    ldx #$1fff              *; Initiliazing the SP at the top of the SRAM
-    txs            
-    
-    ldab #BUFFER_SIZE_MAX   *; Storing the buffer size at ACCB
+    org $1C00               *; Start address of the program in the EPROM
+
+_init_sp:
+    ldx SP_ADDR             *; Store the SP address in register X
+    txs                     *; Initialize the SP with the value in register X
 
 *; Main loop
 _main_loop:
 
-; TODO: Get the validation for RX out
-_rx_Loop:
-    ldaa MCR                *; Reading the MODEM Control Register 
-    cmpa #$04               *; Checking if that the recieving has ended
-    beq _tx_Loop            *; Exit the recieving loop and go to the transmiting loop
+    jsr _push_vars_stack    *; Push the vars/flags into the stack with default values
 
-    jsr _wait_rda           *; Wait for data to be recieved
-    jsr _hdl_irf_rda        *; Handle Recieved Data Available
-
-    jsr _val_irf_err_rx      *; Checking for communication error
-    bra _rx_Loop
+    jsr _rx_Loop
 
 *; TODO: Implement logic for _tx_Loop
-_tx_Loop:
-    nop
+    jsr _tx_Loop
 
     bra _main_loop           *; Continue waiting for next data 
 
-*; Signaling that the buffer is filled
-_hdl_buffer_full:   
-    ldaa #$01               *; Setting /DTR to low to indicate that the buffer is full 
-    staa MCR
-    bra _hdl_buffer_full
+*; TODO: Decide on the question below and finish the subroutine
+_push_vars_stack:
+    ldaa #buffer_curr_cap   *; Storing the current buffer capacity in the stack
+*;? TODO: staa ADDRESS or psha? 
+*;? The concern here is that with staa ADDRESS
+*;? there will be no future confusion about how the variables/flags are stored 
+*;? and more can be added with the only condition they are directly after the last one alredy defined 
+*;? but with this approach it is not so clear where the vars/flags are stored (currently in the stack)
+    staa BUFFER_C_C_ADDR
+
+    ldaa #buffer_full       
+
+
+    ldaa #rx_done
+
+
+    ldaa #tx_done
+
+
+    rts                     *; Return from subroutine
+
+_rx_Loop:
+    jsr _wait_rda           *; Wait for data to be recieved
+    jsr _hdl_irf_rda        *; Handle Recieved Data Available
+
+    jsr _val_irf_err_rx     *; Check for communication error
+
+    ldaa RX_DONE_ADDR       
+    cmpa #01                *; Check if recieving is done
+    beq _rts_rx_loop
+
+    bra _rx_Loop
+
+_rts_rx_loop:
+    rts
 
 *; Wait until interrupt flag for RDA rises
 _wait_rda:
@@ -73,21 +104,15 @@ _hdl_irf_rda:
 
     ldaa #$04               *; Set /OUT1 to low and /OUT2 to high to indicate end of RX
     staa MCR                *; Store in MCR
-; TODO: Check if needed to branch to _rts_hdl_irf_rda or leave it as it is now
+
 _rts_hdl_irf_rda:
     rts                     *; Return from subroutine    
 
-*; Waits until the interrupt flag THRE is raised
-_wait_thre:
-    ldaa IIR                *; Read Interrupt Identification Register
-    cmpa #UART_CODE_THRE    *; Check if TX holding register is empty
-    bne _wait_thre          *; Wait until empty
-
-    rts                     *; Return from subroutine
-
-*; Handle Transmiter Holding Register Empty
-_hdl_irf_thre:
-
+*; Signaling that the buffer is filled
+_hdl_buffer_full:   
+    ldaa #$01               *; Setting /DTR to low to indicate that the buffer is full 
+    staa MCR
+    bra _hdl_buffer_full
 
 *; Error validation for IRF for communication error
 _val_irf_err_rx:
@@ -101,7 +126,18 @@ _val_irf_err_rx:
 *; Handle RX communication error 
     ldaa LSRg               *; Read Line Status Register
     clra                    *; Clear accumulator (error handling can be improved)
-    bra _rts_val_err_rx
 
 _rts_val_err_rx:
     rts
+
+*; Waits until the interrupt flag THRE is raised
+_wait_thre:
+    ldaa IIR                *; Read Interrupt Identification Register
+    cmpa #UART_CODE_THRE    *; Check if TX holding register is empty
+    bne _wait_thre          *; Wait until empty
+
+    rts                     *; Return from subroutine
+
+*; Handle Transmiter Holding Register Empty
+_hdl_irf_thre:
+    nop
